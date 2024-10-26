9535d0a5630d76253db741865d3b213309a9582e
JM Dev <juanma261005@gmail.com>
2024-10-26 16:36:06 -0300

    cambios menores

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://python-vercel-task.vercel.app/"]
+ALLOWED_HOSTS = ['https://python-vercel-task.vercel.app/']
 
 
 # Application definition
