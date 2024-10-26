cc713bc690d1f80056ad4d4be8e0f3c290ac6fb5
JM Dev <juanma261005@gmail.com>
2024-10-26 16:24:28 -0300

    cambio a produccion

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://python-vercel-task.vercel.app/"]
 
 
 # Application definition
